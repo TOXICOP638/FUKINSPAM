c7dfd95e5ddf1214df9cbe62a68d414c22529428
TOXICOP638 <149950962+TOXICOP638@users.noreply.github.com>
2024-04-02 11:49:06 +0530

    Update logs.py

--- OXYBOT/modules/logs.py
+++ OXYBOT/modules/logs.py
@@ -42,7 +42,7 @@ async def logs(legend):
         fetch = await legend.reply(f"__Fetching Logs...__")
     
         with open("OXYBOTLogs.txt", "w") as logfile:
-            logfile.write("⚡ ™°‌ 🫧 🇴 🇽 𝐘 𝐆 𝐄 𝐍 ⚡ [ Bot Logs ]\n\n" + logs)
+            logfile.write("☢️ ™°‌ 𝗧𝗢𝗫𝗜𝗖-𝗫𝗗 ☢️ [ Bot Logs ]\n\n" + logs)
 
         end = datetime.now()
         ms = (end-start).seconds
